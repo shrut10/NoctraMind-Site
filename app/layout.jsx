1822b4789975719dd14e1d0da5de2ba3c333f341
wphs3147-uol <wphs3147@leeds.ac.uk>
2025-11-16 17:30:57 +0000

    Debugging the extra metadata stuff

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,11 +3,6 @@ import "./styles/globals.css";
 
 import ClientLayout from "./ClientLayout";
 
-export const metadata = {
-  title: "Jayashruthi Rajesh Babu",
-  description:
-    "This site is my personal lab for my works and findings on AI and consciousness.",
-};
 
 export default function RootLayout({ children }) {
   return (
