b5555999f5fd5aca78f4fbc314dcd7884797587a
wphs3147-uol <wphs3147@leeds.ac.uk>
2025-11-16 18:09:08 +0000

    Added sitemap

---
+++ app/sitemap.js
@@ -0,0 +1,25 @@
+export default function sitemap() {
+    return [
+      {
+        url: "https://jayashruthi.com",
+        lastModified: new Date(),
+      },
+      {
+        url: "https://jayashruthi.com/projects",
+        lastModified: new Date(),
+      },
+      {
+        url: "https://jayashruthi.com/blog",
+        lastModified: new Date(),
+      },
+      {
+        url: "https://jayashruthi.com/contact",
+        lastModified: new Date(),
+      },
+      {
+        url: "https://jayashruthi.com/about",
+        lastModified: new Date(),
+      },
+    ];
+  }
+  
